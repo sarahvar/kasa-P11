76fca9132568786208ee018783453954130bdd90
sarahvar <sarahvarin95@gmail.com>
2023-08-05 23:45:04 +0200

    corrections-erreurs

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import { useParams } from "react-router-dom";
+import {Navigate, useParams } from "react-router-dom";
 import logements from "../datas/logements.json";
 import Carousel from "../components/carousel";
 import Collapsible from "../components/collapse";
@@ -11,10 +11,14 @@ import Footer from "../components/Footer";
 function Logement() {
   const { id } = useParams();
   const logement = logements.find((logement) => logement.id === id);
+  if (logement === undefined) {
+    return <Navigate to='Notfound'/>
+  }
   const { title, location, rating, host, equipments, description, pictures } =
     logement;
   const isHostSplited = host.name.split(" ");
   const [name, lastname] = isHostSplited;
+
 
   return (
     <>
